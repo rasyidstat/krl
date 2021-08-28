99db5d613854a6f773d2c9f3dacb3453de8dfe73
akherlan <herlandi.27@gmail.com>
2021-08-28 16:26:06 +0700

    update krl data generated by krl_01-02 (get, route, station)

--- krl_02_station.R
+++ krl_02_station.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # read data
-df_krl <- readRDS("krl_detail.rds")
+df_krl <- readRDS("data/krl_detail.rds")
 
 df_krl$route_info[[1]]
 
@@ -12,26 +12,26 @@ map(df_krl$route_info, "tracks")
 
 # create halte data
 df_krl <- df_krl %>%
-  as.tibble() %>%
+  as_tibble() %>%
   mutate(halte_detail = map(df_krl$route_info, "stops"))
 
 df_halte <- df_krl %>%
   select(schedule_id = scheduleId,
          transport_id = transportId,
          validity,
-         name, long_name = longName, color,
+         short_name = name, long_name = longName, color,
          halte_detail) %>%
-  unnest() %>%
+  unnest(cols = c(halte_detail)) %>%
   rename(halte_id = id,
-         halte_name = name1,
+         halte_name = name,
          area_name = areaName,
          direction_name = directionName)
 
 df_halte_final <- df_halte %>%
   group_by(halte_id, halte_name, lat, lng) %>%
   summarise(route_cnt = n_distinct(schedule_id),
-            schedule_id = list(unique(schedule_id))) %>%
-  ungroup() %>%
+            schedule_id = list(unique(schedule_id)),
+            .groups = "drop") %>%
   rename(latitude = lat, longitude = lng)
 
 # finalization
